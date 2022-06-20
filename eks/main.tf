0736a20d57cefc1d3329e2299d664a028b5c2131
devops- <mvp2612@hotmail.com>
2022-06-20 17:29:53 +0530

    Auto Push Commit

--- eks/main.tf
+++ eks/main.tf
@@ -3,8 +3,8 @@ module "eks" {
   ENV                = var.ENV
   PRIVATE_SUBNET_IDS = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
   PUBLIC_SUBNET_IDS  = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
-  DESIRED_SIZE       = 1
-  MAX_SIZE           = 1
-  MIN_SIZE           = 1
+  DESIRED_SIZE       = 2
+  MAX_SIZE           = 2
+  MIN_SIZE           = 2
 }
 
