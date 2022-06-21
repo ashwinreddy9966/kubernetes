8127bdf93ab4332ea261115e0121a44fdffb7fa2
devops- <mvp2612@hotmail.com>
2022-06-21 17:15:40 +0530

    Auto Push Commit

--- eks-extras.sh
+++ eks-extras.sh
@@ -3,3 +3,6 @@
 helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
 helm install ingress-controller nginx-stable/nginx-ingress
+
+helm repo add elastic https://helm.elastic.co
+helm install filebeat elastic/filebeat -f filebeat.yml
